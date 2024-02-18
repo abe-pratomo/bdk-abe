77d79f05f055a4097eb4c3b9c8eb0a0668db9644
abe-pratomo <90553934+abe-pratomo@users.noreply.github.com>
2024-02-18 20:30:23 +0700

    Update script.js

--- tugas-3/script.js
+++ tugas-3/script.js
@@ -1,1 +1,14 @@
-
+function output()
+{
+  let x = Math.random();
+  if (x%2 == 0)
+  {
+    alert(name1 + " dan " + name2 + " cocok!");
+    return
+  }
+  else
+  {
+    alert(name1 + " dan " + name2 + " tidak cocok :')");
+    return
+  }
+}
