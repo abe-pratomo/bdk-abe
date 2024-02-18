664308adc5645b00dec8100ed9a66a25ffd5af4c
abe-pratomo <90553934+abe-pratomo@users.noreply.github.com>
2024-02-18 20:53:42 +0700

    Update script.js

--- tugas-3/script.js
+++ tugas-3/script.js
@@ -2,7 +2,7 @@ function output() {
   let name1 = document.forms[0].name1.value;
   let name2 = document.forms[0].name2.value;
 
-  let x = Math.random();
+  let x = Math.floor(Math.random() * 1);
   if (x % 2 === 0) {
     document.getElementById("result").innerHTML = x + " " + name1 + " dan " + name2 + " cocok!";
   } else {
