dc3900e06919926c29aa2b99b1f52ca41a69be72
abe-pratomo <90553934+abe-pratomo@users.noreply.github.com>
2024-02-18 21:16:14 +0700

    Update script.js

--- tugas-3/script.js
+++ tugas-3/script.js
@@ -1,17 +1,15 @@
-function output() {
+function output()
+{
   let name1 = document.forms[0].name1.value;
   let name2 = document.forms[0].name2.value;
 
   let x = Math.floor(Math.random() * 2);
-  if (x === 0) {
-    document.getElementById("result").innerHTML = name1 + " dan " + name2 + " cocok!";
-  } else {
-    document.getElementById("result").innerHTML = name1 + " dan " + name2 + " tidak cocok :')";
+  if (x === 0)
+  {
+    alert(name1 + " dan " + name2 + " cocok!");
   }
-}
-
-document.getElementById("myForm").addEventListener("submit", function (event)
-{
-  event.preventDefault();
-  output();
+  else
+  {
+    alert(name1 + " dan " + name2 + " tidak cocok :')");
+  }
 }
